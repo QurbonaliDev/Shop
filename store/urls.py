47b94fe2b33afa2bb5eaa01a20819a9207e02e5e
Mo'minov Qurbonali <mominovqurbonali1201@gmail.com>
2024-12-13 12:16:11 +0500

    add slugify and buy model

--- store/urls.py
+++ store/urls.py
@@ -1,16 +1,9 @@
-from django.templatetags.static import static
 from django.urls import path
 
-from market import settings
-from store.views import home, products, single, register, contact
+from store.views import *
 
-urlpatterns = ([
-    path('', home, name = 'name'),
-    path('contact',contact, name = 'contact'),
-    path('products/<slug>/',products, name = 'products'),
-    path('register',register, name = 'register'),
-    path('single',single, name = 'single'),
-])
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns = [
+    path('', home, name='home'),
+    path('product/<slug>', products, name='products'),
+    path('single/<int:pk>/', single, name='single'),
+]
--- store/views.py
+++ store/views.py
@@ -1,34 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from store.models import Product,Category
+from .forms import ChoiseForm
+from .models import *
 
 
 def home(request):
     ctg = Category.objects.all()
     product = Product.objects.all()
-    ctx={
-        'ctg':ctg,
-        'product':product,
-    }
-    return render(request,'index.html',ctx)
+    ctx = {'ctg': ctg,
+           'product': product}
+    return render(request, 'index.html', ctx)
 
-def contact(request):
-    ctx={}
-    return render(request,'contact.html',ctx)
 
-def products(request,slug=None):
+def products(request, slug=None):
     ctg = Category.objects.all()
-    product = Product.objects.filter(category__slug=slug) if slug else Product.objects.all()
-    ctx={
-        'ctg':ctg,
-        'product':product,
-    }
-    return render(request,'products.html',ctx)
+    category = Category.objects.get(slug=slug)
+    product = Product.objects.all().filter(type_id=category.id)
+    ctx = {'ctg': ctg,
+           'category': category,
+           'product': product}
+    return render(request, 'products.html', ctx)
+
 
 def register(request):
-    ctx={}
-    return render(request,'register.html',ctx)
+    ctx = {}
+    return render(request, 'register.html', ctx)
 
-def single(request):
-    ctx={}
-    return render(request,'single.html',ctx)
+
+def single(request, pk=None):
+    ctg = Category.objects.all()
+    product_pk = Product.objects.get(pk=pk)
+    form = ChoiseForm()
+    if request.POST:
+        forms = ChoiseForm(request.POST or None,
+                           request.FILES or None)
+        if forms.is_valid():
+            root = forms.save()
+            root = Buy.objects.get(pk=root.pk)
+            root.product = product_pk
+            root.save()
+            return redirect('home')
+        else:
+            print(forms.errors)
+    ctx = {'ctg': ctg,
+           'product_pk': product_pk,
+           'form': form}
+    return render(request, 'single.html', ctx)
